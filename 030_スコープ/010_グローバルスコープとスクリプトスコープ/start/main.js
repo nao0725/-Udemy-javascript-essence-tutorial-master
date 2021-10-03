07281e38664f4ebf3338b7315112cd6026243c6a
nao0725 <aqua.725.times@gmail.com>
2021-10-04 08:22:24 +0900

    js　復習

--- 030_スコープ/010_グローバルスコープとスクリプトスコープ/start/main.js
+++ 030_スコープ/010_グローバルスコープとスクリプトスコープ/start/main.js
@@ -1,9 +1,17 @@
+
+// 変数をそれぞれ定義
+// スクリプトスコープ
 let a = 0;
+// グローバルスコープ
 var b = 0;
 function c(){}
+// railsのbyebugみたいなやつ
 
+// グローバルオブジェクトは省略可能
+// 以下のように出力する
 console.log(b);
 
+// 例
+// console.log()で出力する
 window.d = 1;
-let d = 2;
 console.log(d);
