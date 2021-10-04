a3c43d1f2114d9badc143757deccc93e9289f82e
nao0725 <aqua.725.times@gmail.com>
2021-10-04 14:56:58 +0900

    [add] セクション６ falthyな値

---
+++ 040_変数/040_falsyとtruthy/start/main.js
@@ -0,0 +1,20 @@
+let a = 0;
+console.log(Boolean(a));
+
+let b = ""
+console.log(Boolean(b));
+
+let c = NaN;
+console.log(Boolean(c));
+
+ let d = 0n;
+ console.log(Boolean(d));
+
+ let e = parseInt("");
+ console.log(Boolean(e));
+
+ if(a){
+   console.log("hello");
+ } else {
+   console.log("bye");
+ }
