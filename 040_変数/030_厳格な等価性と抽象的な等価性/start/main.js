26c7a4f80e700c5fa2b601449744acdd0853147a
nao0725 <aqua.725.times@gmail.com>
2021-10-04 14:48:02 +0900

    js　セクション6 厳格な等価性と抽象的な等価性

---
+++ 040_変数/030_厳格な等価性と抽象的な等価性/start/main.js
@@ -0,0 +1,20 @@
+ function printEquality(a,b){
+  console.log(a === b);
+  console.log(a == b);
+ }
+ 
+ let a = "1";
+ let b = 1;
+ printEquality(a,b);
+
+ let c = true;
+//  console.log ( b===Number(c)); 
+//  printEquality(b,c)
+
+ let e = "";
+ let f = 0;
+//  printEquality(e,f);
+
+
+ let g = "0";
+ printEquality(f,g);
