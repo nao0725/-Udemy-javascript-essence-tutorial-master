21d1d3dbd81a1b166631d2b950484aa9aee55af7
nao0725 <aqua.725.times@gmail.com>
2021-10-05 08:29:14 +0900

    [finish] スコープチェーンの復習

--- 030_スコープ/040_スコープチェーン/start/main.js
+++ 030_スコープ/040_スコープチェーン/start/main.js
@@ -1,18 +1,17 @@
+// スクリプトスコープ
 let a = 2;
+// グローバルスコープ
 window.a = 4;
-function fn1() {
+function fn1(){
     // let a = 1;
-    function fn2() {
+    function fn2(){
         console.log(a);
-
-        if (true) {
+        if (true){
             var a = 3;
         }
-
+        // var = 3;のホスティングが働いてしまうのでundifinedと出力される
+        // ホスティングとはコンテキスト内で宣言した変数や関数の定義をコード実行前にメモリーに配置すること
     }
     fn2();
 }
 fn1();
-
-
-
