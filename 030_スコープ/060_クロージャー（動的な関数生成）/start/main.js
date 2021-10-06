eb64ba0add41df40b63c556149381b9a7ca9310f
nao0725 <aqua.725.times@gmail.com>
2021-10-06 09:34:38 +0900

    動的な関数の生成の復習

--- 030_スコープ/060_クロージャー（動的な関数生成）/start/main.js
+++ 030_スコープ/060_クロージャー（動的な関数生成）/start/main.js
@@ -1,17 +1,21 @@
 
-function incrementFactory(){
-
-  let num = 0;
-
-  function increment(){
-    num = num + 1;
-    console.log(num);
+// 関数addNumberFactory内に更に関数addNumberを定義
+// 更にそれぞれに引数num,valをもたせる
+function addNumberFactory(num){
+  function addNumber(value){
+    // 戻り値の作成
+    return num + value;
   }
-  return increment;
+  // addNumberFactoyにaddNumberを返す
+  return addNumber;
 }
 
-const increment = incrementFactory();
+// add5変数にnumに５を代入した関数addNumberFactoryを定義
+const add5 = addNumberFactory(5);
+// 更にその値を変数resultに引数valを10にして代入
+// 変数numに5が設定されている + value10を渡すので15と出力される
+const result = add5(10);
+// このようにすると15と出力される
+console.log(result);
 
-increment();
-increment();
-increment();
+// このように渡す値によって生成される関数が変わってくる
