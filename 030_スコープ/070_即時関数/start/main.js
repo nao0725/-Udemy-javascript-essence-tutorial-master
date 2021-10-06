4cfea212c4bbbe3a8052553aef2eb6ab731d1e20
Naoyuki Aizawa <78492401+nao0725@users.noreply.github.com>
2021-10-07 08:55:22 +0900

    【JS】復習

--- 030_スコープ/070_即時関数/start/main.js
+++ 030_スコープ/070_即時関数/start/main.js
@@ -1,33 +1,27 @@
-function a(){
- console.log("called")
-}
-
-a();
-
 let c = (function(){
-
   console.log("called");
+  // 変数privateValを定義 関数内でしか使えない
+  let privateVal　 = 0;
+  // 変数publicValを定義　これは関数の外で使えるようにしたい
+  let publicVal = 10;
 
-let privateVal = 0;
-let publicVal = 10;
+  // 関数も上記の様に生成
+  function privateFn(){
+    console.log("privateFn is called");
+  }
+  function publicFn(){
+    console.log("publicFn is called");
+  }
 
-function privateFn(){
-  console.log("privateFn is called");
-}
-
-function publicFn(){
-  console.log("publicFn is called" + privateVal++);
-}
-
-  
+  // 戻り値を設定してこの結果が変数cに格納される
   return {
-    privateVal;
-    publicVal;
+    privateVal,
+    publicFn
   };
 })()
 
-console.log(c);
+// 関数の外から下記のように呼び出すとコンソールに出力される
+c.publicFn();
+console.log(c.publicVal);
 
-let b = function(){
-  console.log("called");
-}();
+// c.privateValと書くことはできない
