be701046e5e2e3b73e4331b983f16628431a8077
nao0725 <aqua.725.times@gmail.com>
2021-10-09 08:01:37 +0900

    [復習] JS　暗黙的型変換

--- 040_変数/020_暗黙的な型変換/start/main.js
+++ 040_変数/020_暗黙的な型変換/start/main.js
@@ -1,21 +1,38 @@
+// データ型を調べる関数を用意
 function printTypeAndValue(val){
+  // typeofメソッド（？）を入れるとデータ型がわかる
   console.log(typeof val, val)
 }
 
+// 変数を定義
 let a = 0;
+// number 0と出力される
 printTypeAndValue(a);
 
+// 文字列　＋　数値
 let b = "1" + a;
+// この場合Stringの型で出力
+// データ型が違う場合はどちらかのデータ型が出力される
 printTypeAndValue(b);
 
+// 数値　ー　文字列
 let c = 15 - b;
+// -は数値の計算にしか使われない！なので、15-10で5が出力
 printTypeAndValue(c);
 
+// nullとは参照を保持しない＝空ということ
 let d = c - null
+// nullが暗黙的に0に変換される 5がそのまま保持
 printTypeAndValue(d);
 
+// tureは数値の計算で使用された場合は０扱いになる
 let e = d - true
+// 数値の４が出力 
 printTypeAndValue(e);
 
+// 明示的に型変換をする
+// 文字列を数値へ
+// parseInt（文字列）で数値に変換
 let g = parseInt("1") + a;
+// number 1 と出力される
 printTypeAndValue(g);
