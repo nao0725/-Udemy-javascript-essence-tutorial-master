a5c68348f152f31187591b4b6450fd21c1d02a2b
nao0725 <aqua.725.times@gmail.com>
2021-10-10 08:55:51 +0900

    [add] JS　復習　falseyとturethy

--- 040_変数/040_falsyとtruthy/start/main.js
+++ 040_変数/040_falsyとtruthy/start/main.js
@@ -1,3 +1,4 @@
+// 0なのでfalseと等価
 let a = 0;
 console.log(Boolean(a));
 
@@ -9,10 +10,17 @@ console.log(Boolean(c));
 
  let d = 0n;
  console.log(Boolean(d));
+ //  上記はすべてコンソールでfalseとして出力される
 
  let e = parseInt("");
- console.log(Boolean(e));
+ console.log(e);
+//  上記はparseIntで整数として定義しているが空文字は数字に置き換えられないので、
+// 出力結果はNan(Not a Number)となる
+// よって下記の場合はfalseと出力される
+// console.log(Booolean(e));
 
+// 変数がfalseyかどうかif文で確認する
+// もしaを下記のif文にかけて実行するとfalseなので"bye"と出力される
  if(a){
    console.log("hello");
  } else {
