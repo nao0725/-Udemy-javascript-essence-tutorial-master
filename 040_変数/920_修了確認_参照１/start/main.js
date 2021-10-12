7a5a77b710e40b7d2b0d00ad4545ed05fa70c3c8
nao0725 <aqua.725.times@gmail.com>
2021-10-12 15:36:51 +0900

    [add] JS セクション６　参照

--- 040_変数/920_修了確認_参照１/start/main.js
+++ 040_変数/920_修了確認_参照１/start/main.js
@@ -7,7 +7,8 @@ let val = 0;
 let val2 = val;
 val2 = 1;
 
-// console.log(val); // 問題１
+// 問題１
+console.log(val);
 
 let obj = {
     prop1: 10
@@ -15,15 +16,16 @@ let obj = {
 const obj2 = obj;
 obj2.prop1 = 9;
 
-// console.log(obj); // 問題２
+ // 問題２
+console.log(obj);
 
 function fn1(arg) {
     arg.prop2 = 8;
     return arg;
 }
 let obj3 = fn1(obj2);
-
-// console.log(obj); // 問題３
+ // 問題３
+console.log(obj);
 
 function fn2(arg) {
     arg = {};
@@ -31,4 +33,4 @@ function fn2(arg) {
 }
 const obj4 = fn2(obj); // 問題４
 
-// console.log(obj); // 問題４
+console.log(obj); // 問題４
