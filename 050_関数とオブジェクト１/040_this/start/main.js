e6283e313f831ae4cfc3ab7d24a499d651d0d677
nao0725 <aqua.725.times@gmail.com>
2021-10-13 17:46:13 +0900

    [add] JS セクション7 this

--- 050_関数とオブジェクト１/040_this/start/main.js
+++ 050_関数とオブジェクト１/040_this/start/main.js
@@ -1,6 +1,15 @@
+// this
+// 関数内で使える
+// 呼び出し元のオブジェクトへの参照を保持するキーワード
+
 const person = {
     name: 'Tom',
     hello: function() {
-
+        console.log("Hello" + this.name);
     }
 }
+
+person.hello();
+
+
+// thisの参照先は実行コンテキストによって変わる
