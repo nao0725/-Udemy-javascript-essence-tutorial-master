fb97d3b610799a86bb53a4ffd74aee6b3a2f83fd
nao0725 <aqua.725.times@gmail.com>
2021-10-13 17:55:37 +0900

    [add] JS セクション7  参照とコピーとthis

--- 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
+++ 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
@@ -1,7 +1,34 @@
+// window.name = "John";
+
+// const person = {
+//     name: 'Tom',
+//     hello: function() {
+//         console.log('Hello ' + this.name);
+//     }
+// }
+// const ref = person.hello;
+// ref();
+
+window.name = "John";
+
 const person = {
     name: 'Tom',
     hello: function() {
         console.log('Hello ' + this.name);
+        a();
+
+        const person = {
+            name: 'Tim',
+            hello: function() {
+                console.log('Hello ' + this.name);
+                a();
+            }
+        }
+        person.hello();
     }
 }
 person.hello();
+
+function a(){
+    console.log('Hello ' + this.name);
+}
