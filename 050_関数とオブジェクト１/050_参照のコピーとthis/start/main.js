d3e21bd0c5038995861522eee409c4a9e3c4825c
nao0725 <aqua.725.times@gmail.com>
2021-10-18 08:57:34 +0900

    [add] JS 復習 thisと参照のコピー

--- 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
+++ 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
@@ -1,34 +1,29 @@
-// window.name = "John";
-
-// const person = {
-//     name: 'Tom',
-//     hello: function() {
-//         console.log('Hello ' + this.name);
-//     }
-// }
-// const ref = person.hello;
-// ref();
-
 window.name = "John";
-
 const person = {
     name: 'Tom',
     hello: function() {
         console.log('Hello ' + this.name);
         a();
 
+        // さらにpersonを定義する
         const person = {
-            name: 'Tim',
+            // nameの値を「太郎」に変更
+            name: '太郎',
             hello: function() {
                 console.log('Hello ' + this.name);
+                // ここでは上記同様に、Johnが呼ばれる
+                // **関数**として実行される場合(26行目)
                 a();
             }
         }
+        // 再定義したpersonを実行すると"太郎"が呼ばれる
+        // **オブジェクト**のメソッドとして実行される場合
         person.hello();
     }
 }
-person.hello();
 
 function a(){
     console.log('Hello ' + this.name);
 }
+
+person.hello();
