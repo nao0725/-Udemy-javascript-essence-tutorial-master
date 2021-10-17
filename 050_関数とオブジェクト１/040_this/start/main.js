e3eecf6ca275463befaf4ec2241e1777b7ba68c3
nao0725 <aqua.725.times@gmail.com>
2021-10-18 08:20:39 +0900

    [add] JS 復習 htis

--- 050_関数とオブジェクト１/040_this/start/main.js
+++ 050_関数とオブジェクト１/040_this/start/main.js
@@ -2,14 +2,20 @@
 // 関数内で使える
 // 呼び出し元のオブジェクトへの参照を保持するキーワード
 
+// personというオブジェクトを設定
 const person = {
+    // nameとhelloをそれぞれ設定
     name: 'Tom',
     hello: function() {
+        // この場合,thisがpersonを参照しているpersonオブジェクトの中のnameを呼び出す
         console.log("Hello" + this.name);
     }
 }
-
+// helloを実行「Hello Tom」と出力
 person.hello();
+
+// helloで定義した関数内からみて外部の関数スコープ
+// 　＝レキシカルスコープがpersonにあたるので値を取得できる
 
 
 // thisの参照先は実行コンテキストによって変わる
