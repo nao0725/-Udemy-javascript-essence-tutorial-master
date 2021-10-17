8723b522eee1437f0aa01e6654d33f408e003c32
nao0725 <aqua.725.times@gmail.com>
2021-10-17 10:58:20 +0900

    [add] JS 復習 関数とオブジェクト

--- 050_関数とオブジェクト１/030_コールバック関数/start/main.js
+++ 050_関数とオブジェクト１/030_コールバック関数/start/main.js
@@ -1,51 +1,36 @@
-// 関数は実行可能なオブジェクトである
-
-// コールバック関数
-// 他の関数に引数として渡される関数
-
-// function hello(){
-//   console.log("hello");
-// }
-
-// function fn(cb){
-//   cb();
-// }
-
-// fn(hello);
-
-
-// function hello(){
-//     console.log("hello");
-//   }
-
-//   function bye(){
-//     console.log("bye");
-//   }
-  
-//   function fn(cb){
-//     cb();
-//   }
-  
-//   fn(hello);
-//   fn(bye);
-
-
-function hello(){
-  console.log("hello");
+// 引数に名前を設定する
+// helloの引数にnameを設定
+function hello(name){
+  // コンソールにhello + nameが出力される
+  console.log("hello" + name);
 }
 
 function bye(){
-  console.log("bye");
+  console.log("bye")
 }
 
 function fn(cb){
+  // 引数に呼び出される名前を設定
   cb("Tom");
 }
 
 fn(hello);
 fn(bye);
+
+// 宣言する際の実引数と実行する際の仮引数が一致している必要はない
+// だからbyeに影響を受けていない
+
+// 無名関数でも定義が可能
+// helloと実行される
+fn(function (){
+  console.log("hello");
+})
+// 引数を入れて実行することも可能
 fn(function(name){
-  console.log("hello" + name);
+  console.log("bye" + name);
 })
 
+// setTimeoutはJavascript側で用意されているWebAPIの１つ
 setTimeout(hello, 2000);
+// ここでは実引数がないため「hello undefined」と出力される
+// 2000はミリ秒数　コンソールに２秒遅れて出力される
