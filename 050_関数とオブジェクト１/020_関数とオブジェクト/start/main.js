09b166bb96f25975e9b28512f75eb19dda0070c2
nao0725 <aqua.725.times@gmail.com>
2021-10-17 10:26:12 +0900

    [add] JS 復習 関数とオブジェクト

--- 050_関数とオブジェクト１/020_関数とオブジェクト/start/main.js
+++ 050_関数とオブジェクト１/020_関数とオブジェクト/start/main.js
@@ -1,14 +1,21 @@
-// 関数は実行可能なオブジェクトである
-
+// helloと出力される関数を用意
 function a() {
   console.log("hello");
 }
-
+// propには0を、methodでは「メソッド」と出力されるように設定
 a.prop = 0;
 a.method = function(){
   console.log("メソッド")
 }
 
+// 実行すると下記のようにそれぞれ出力される
+// hello
 a();
+// メソッド
 a.method();
+// 0
 console.log(a.prop);
+
+// このように関数宣言によって宣言した関数を
+// dot記法を使ってプロパティやメソッドが使用できる
+// ()をつけると関数が実行される
