bd323487980fa23dd901e548f89ba01d72f77cc9
nao0725 <aqua.725.times@gmail.com>
2021-10-18 17:52:24 +0900

    [add] JS　終了問題2

--- 050_関数とオブジェクト１/910_修了確認_コールバック関数/start/main.js
+++ 050_関数とオブジェクト１/910_修了確認_コールバック関数/start/main.js
@@ -8,11 +8,14 @@
  */
 const person = {
     hello: function () {
-        console.log("helloTom")
+        return "hello Tom"
     }
 }
 
-setTimeout(person.hello, 1000);
+setTimeout(function (){
+    const hello = person.hello();
+    console.log(hello);
+} ,1000);
 
 /**
  * 問題２：
@@ -27,7 +30,10 @@ setTimeout(person.hello, 1000);
 
 // setTimeout(alert("helloTom",1000));
 
-
+// setTimeout(function (){
+//     const hello = person.hello();
+//     alert(hello);
+// } ,1000);
 
 /**
  * 問題３：
@@ -66,32 +72,32 @@ obj.greeting = function() {
  * 
  * ※コールバック関数を用いて実装してください。
  */
-function calcFactory(val) {
+function calcFactory(val, callback) {
     return {
         plus: function(target) {
             const newVal = val + target;
-            console.log(`${val} + ${target} = ${newVal}`);
+            callback(`${val} + ${target} = ${newVal}`);
             val = newVal;
         },
         minus: function(target) {
             const newVal = val - target;
-            console.log(`${val} - ${target} = ${newVal}`);
+            callback(`${val} - ${target} = ${newVal}`);
             val = newVal;
         },
         multiply: function(target) {
             const newVal = val * target;
-            console.log(`${val} x ${target} = ${newVal}`);
+            callback(`${val} x ${target} = ${newVal}`);
             val = newVal;
         },
         divide: function(target) {
             const newVal = val / target;
-            console.log(`${val} / ${target} = ${newVal}`);
+            callback(`${val} / ${target} = ${newVal}`);
             val = newVal;
         }
     };
 }
 
-const calc = calcFactory(10);
+const calc = calcFactory(10, alert);
 calc.plus(5); 
 calc.minus(3); 
 calc.multiply(3);
