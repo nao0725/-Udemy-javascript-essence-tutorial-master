6e3b0dc7ba0430e4a301f50f0c3347728195e0b2
nao0725 <aqua.725.times@gmail.com>
2021-10-19 08:43:36 +0900

    [add] JS　復習 コールバック関数とthis

--- 050_関数とオブジェクト１/060_コールバック関数とthis/start/main.js
+++ 050_関数とオブジェクト１/060_コールバック関数とthis/start/main.js
@@ -8,9 +8,18 @@ const person = {
 }
 person.hello();
 
-
+// refという引数にコールバック関数が渡ってくるようにする
 function fn(ref){
     ref();
 }
 
-fn(person.hello)
+// 実行結果は「hello John」と出力される(関数として実行しているから？)
+fn(person.hello);
+
+// メソッドを他の変数に代入していることと同じ
+// refに渡ってくるのはpersonのhelloというプロパティが参照している先の関数
+// refを実行するということは関数を実行していることになる
+// 関数として実行した場合,thisにはグローバルオブジェクトの値が格納される(window)
+
+
+
