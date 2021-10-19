ff665d7f5f35a95cac6affa6ffd8d827c6df76da
nao0725 <aqua.725.times@gmail.com>
2021-10-20 08:35:09 +0900

    [add] js 復習 bind

--- 050_関数とオブジェクト１/070_bindとthis/start/main.js
+++ 050_関数とオブジェクト１/070_bindとthis/start/main.js
@@ -1,4 +1,4 @@
-window.name = 'John';
+// window.name = 'John';
 
 const person = {
     name: 'Tom',
@@ -6,21 +6,16 @@ const person = {
         console.log('Hello ' + this.name);
     }
 }
-person.hello();
-
-const helloTom = person.hello.bind(person);
-
-function fn(ref) {
-    ref();
-}
-
-fn(helloTom);
-
-
+// 引数にnameを入れる（中身のthisを削除した）
 function a(name){
-    console.log("hello" + name);
+    console.log("hello " + name);
 }
+// 関数内でthisが使用されていないので,第一引数は必要ない => nullとしておく
+// 第２引数として関数の引数を渡すことでbindをかけられる
+const b = a.bind(null, "次郎");
+// 「hello 次郎」と出力
+b();
+// もし、bに引数を渡したとしてもbindが優先される
+b("三郎");
 
-const b = a.bind(null, "Tim");
 
-b("Tom");
