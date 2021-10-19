6eb911398ae34eeb68f6578278bec24324479598
nao0725 <aqua.725.times@gmail.com>
2021-10-19 18:52:53 +0900

    [add] js 関数とオブジェクト　instanceof

--- 060_関数とオブジェクト２/040_instanceof/start/main.js
+++ 060_関数とオブジェクト２/040_instanceof/start/main.js
@@ -1,7 +1,28 @@
+// instanceofメソッド
+// どのコンストラクターから生成されたオブジェクトか確認する
+
 function F(a, b) {
     this.a = a;
     this.b = b;
-    // return {a: 1};
+    const result = new Object();
+    result.a = 1;
+    return result;
 }
 
 F.prototype.c = function() {}
+
+const instance = new F(1,2)
+console.log(instance instanceof Object);
+console.log(instance.__proto__ === Object.prototype)
+
+
+function fn(arg){
+    if(arg instanceof Array){
+        arg.push("value")
+    } else {
+        arg["key"] = "value";
+    }
+    console.log(arg)
+}
+
+fn([]);
