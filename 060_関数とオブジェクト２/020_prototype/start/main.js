8501016540ed7fe97881bf03b816850342e7a9ae
nao0725 <aqua.725.times@gmail.com>
2021-10-19 18:03:41 +0900

    [add] js 関数とオブジェクト　prototypw

--- 060_関数とオブジェクト２/020_prototype/start/main.js
+++ 060_関数とオブジェクト２/020_prototype/start/main.js
@@ -3,6 +3,24 @@ function Person(name, age) {
     this.age = age;
 }
 
+// Person.prototype.hello = function(){
+//     console.log("Hello" + this.name);
+// }
+
 const bob = new Person('Bob', 18);
 const tom = new Person('Tom', 33);
 const sun = new Person('Sun', 20);
+
+bob.hello();
+tom.hello();
+sun.hello();
+
+// =============================================
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.hello = function(){
+        console.log("Hello" + this.name);
+    }
+}
