e804df69057ca2e39f0d99da6aab995aac111de8
nao0725 <aqua.725.times@gmail.com>
2021-10-22 09:11:14 +0900

    [add] js　復習  アロー関数

--- 050_関数とオブジェクト１/090_アロー関数/start/main.js
+++ 050_関数とオブジェクト１/090_アロー関数/start/main.js
@@ -1,23 +1,39 @@
-// アロー関数
-// 無名関数を記述しやすくした省略記法
-
+// 一般的な関数
 function a(name){
-  return "hello" + name;
+  return "hello " + name;
 }
 
-// const b = function(name){
-//   return "hello" + name;
-// }
-
-
-// const b = name =>  "hello" + name;
-// console.log(b(" Tom"));
-
-
-const c = (name2, name3) => "hello" + name2 + " " + name3;
-console.log(c("Tom", "Bob")); 
-
-const b = () => {
-  return "hello";
+// 関数式にした場合
+const b = function(name){
+  return "hello " + name;
 }
-console.log (b("Tom", "Bob"))
+
+// 上記をアロー関数に直すと下記の通り
+const c = name => "hello " + name;
+
+// functionを削除して、{}の前に=>を置く
+// 引数が１つの場合()は省略できる
+// 実行行が一行の場合は{}が省略できる
+// そうすると変数cにアロー関数が代入される
+console.log(c("炭治郎"));
+
+// ()で引数を囲い、第二引数を用意
+const d = (name, name1) => "hello " + name + ", " + name1
+console.log(d("善逸", "伊之助"));
+
+// 引数がなくても()の省略は不可
+const e = () => "hello";
+console.log(e());
+
+// （）ではなく_でも代用可
+const f = _ => "hello";
+console.log(f());
+
+// 複数行ある場合は{}使う
+// {}がある場合はreturn省略不可
+const g = () => {
+  const h = "うまい！" + "うまい！";
+  return h;
+}
+// 実行結果は「うまい！うまい！」となる
+console.log(g())
