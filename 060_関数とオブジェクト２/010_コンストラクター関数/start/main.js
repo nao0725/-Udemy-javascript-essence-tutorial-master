0b2b5bf1f83f43bf8922a9e6833e0ffe42a72cc3
nao0725 <aqua.725.times@gmail.com>
2021-10-24 18:35:30 +0900

    [add] js　復習 コンストラクター関数

--- 060_関数とオブジェクト２/010_コンストラクター関数/start/main.js
+++ 060_関数とオブジェクト２/010_コンストラクター関数/start/main.js
@@ -1,8 +1,23 @@
+// function Person(name,age){
+//   this.name = name;
+//   this.age = age;
+// }
+
+// const bob = new Person("Bob", 18);
+// const tom = new Person("Tom", 19);
+// const sun = new Person("Sun", 20);
+
+
+// 関数を定義
+// 普通の関数と区別するために大文字で定義する
 function Person(name,age){
+  // 格納したいプロパティを定義
   this.name = name;
   this.age = age;
 }
-
-const bob = new Person("Bob", 18);
-const tom = new Person("Tom", 19);
-const sun = new Person("Sun", 20);
+// new演算子を用いてインスタンス化
+const a = new Person("武田", 30);
+console.log(a);
+// 他のオブジェクトも生成
+const b = new Person("双葉", 25);
+const c = new Person("風間", 28);
