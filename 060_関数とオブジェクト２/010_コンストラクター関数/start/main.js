1cbd6f4a34586d37c6ff18936e610bfab6fd0f1b
nao0725 <aqua.725.times@gmail.com>
2021-10-25 10:40:26 +0900

    [add] js　復習 prototype

--- 060_関数とオブジェクト２/010_コンストラクター関数/start/main.js
+++ 060_関数とオブジェクト２/010_コンストラクター関数/start/main.js
@@ -1,11 +1,11 @@
-// function Person(name,age){
-//   this.name = name;
-//   this.age = age;
-// }
+function Person(name,age){
+  this.name = name;
+  this.age = age;
+}
 
-// const bob = new Person("Bob", 18);
-// const tom = new Person("Tom", 19);
-// const sun = new Person("Sun", 20);
+const bob = new Person("Bob", 18);
+const tom = new Person("Tom", 19);
+const sun = new Person("Sun", 20);
 
 
 // 関数を定義
--- 060_関数とオブジェクト２/020_prototype/start/main.js
+++ 060_関数とオブジェクト２/020_prototype/start/main.js
@@ -1,26 +1,26 @@
 function Person(name, age) {
     this.name = name;
     this.age = age;
+    this.hello = function(){
+        console.log("Hello" + this.name);
+    }
 }
-
-// Person.prototype.hello = function(){
-//     console.log("Hello" + this.name);
-// }
 
 const bob = new Person('Bob', 18);
 const tom = new Person('Tom', 33);
 const sun = new Person('Sun', 20);
 
+// 実行するとHelloBobと出力
 bob.hello();
 tom.hello();
 sun.hello();
 
 // =============================================
 
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
-    this.hello = function(){
-        console.log("Hello" + this.name);
-    }
-}
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+//     this.hello = function(){
+//         console.log("Hello" + this.name);
+//     }
+// }
