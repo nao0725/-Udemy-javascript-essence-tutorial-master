4314198836fbb72423d8a3065a8104853161428d
nao0725 <aqua.725.times@gmail.com>
2021-10-27 09:25:19 +0900

    [add] js　復習 instanceof

--- 060_関数とオブジェクト２/040_instanceof/start/main.js
+++ 060_関数とオブジェクト２/040_instanceof/start/main.js
@@ -1,28 +1,26 @@
-// instanceofメソッド
-// どのコンストラクターから生成されたオブジェクトか確認する
+// function F(a,b){
+//     this.a = a;
+//     this.b = b;
+//     return {a:1};
+// }
 
-function F(a, b) {
-    this.a = a;
-    this.b = b;
-    const result = new Object();
-    result.a = 1;
-    return result;
-}
-
-F.prototype.c = function() {}
-
-const instance = new F(1,2)
-console.log(instance instanceof Object);
-console.log(instance.__proto__ === Object.prototype)
-
+// F.prototype.c = function(){}
+// const instance = new F(1,2);
+// console.log(instance instanceof F);
+// console.log(instance.__proto__ === Object.prototype);
 
 function fn(arg){
-    if(arg instanceof Array){
-        arg.push("value")
+    if (arg instanceof Array){
+        // 条件分岐でArrayが配列の場合
+        arg.push("value");
     } else {
+        // 条件分岐で配列以外の場合（オブジェクト）
         arg["key"] = "value";
     }
-    console.log(arg)
+    // argの結果が返ってくるように設定
+    console.log(arg);
 }
-
+// 配列の場合：['value']と出力され0番にvalueが入っている
 fn([]);
+// オブジェクトの場合：{key: 'value'}と実行されオブジェクトにkey: "value"と値が入っている
+fn({});
