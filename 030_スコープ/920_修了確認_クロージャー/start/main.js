a7deb69d825423981766dfd383f622e13133f511
nao0725 <aqua.725.times@gmail.com>
2021-10-28 16:10:19 +0900

    [add] js　終了確認2回目 クロージャー

--- 030_スコープ/920_修了確認_クロージャー/start/main.js
+++ 030_スコープ/920_修了確認_クロージャー/start/main.js
@@ -16,34 +16,4 @@
  * ※四則演算は"+","-","*","/"を数値ではさんで計算を行います。
  */
 
-function calcFactory(val){
-  return{
-    plus: function(target){
-      const newVal = val + target;
-      console.log(val + "+" + target + "=" + newVal)
-      val = newVal;
-    },
-    minus: function(target){
-      const newVal = val - target;
-      console.log(`${val} + "-" + ${target} + "=" + ${newVal}`)
-      val = newVal;
-    },
-    multiply: function(target){
-      const newVal = val * target;
-      console.log(val + "×" + target + "=" + newVal)
-      val = newVal;
-    },
-    divide: function(target){
-      const newVal = val / target;
-      console.log(val + "÷" + target + "=" + newVal)
-      val = newVal;
-    }
-  };
-}
-
-const calc = calcFactory(10);
-calc.plus(5);
-calc.minus(3);
-calc.multiply(3);
-calc.divide(2);
-
+// ２回目：全くわからんかった、、、、(ヒント：計算式をそれぞれ変数で置き換えよう)
