d2fe4308c628e1726b49adfe65cd6ff374013a57
nao0725 <aqua.725.times@gmail.com>
2021-10-29 08:43:55 +0900

    [add] js　inとhasOwnProperty

--- 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
+++ 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
@@ -8,3 +8,14 @@ Object.prototype.hello = function() {
 }
 
 const bob = new Person('Bob', 18);
+const result = bob.hasOwnProperty("age");
+console.log(result);
+// bobオブジェクトの中に"引数"に対応するプロパティはあるか確認できる
+console.log("name" in bob);
+// この場合プロトタイプチェーンも遡るので、trueと返ってくる
+console.log("hello" in bob);
+// もちろん存在しないものはfalse
+console.log("bye" in bob);
+// Objectのプロトタイプに格納されているhasOwnPropertyもプロトタイプチェーンによって格納されているのでtrue
+console.log("hasOwnProperty" in bob);
+
