f6f9128d9e4a7958467b7e3ee3016abb1039647b
nao0725 <aqua.725.times@gmail.com>
2021-10-29 08:24:46 +0900

    [add] js　プロトタイプチェーン

--- 060_関数とオブジェクト２/060_プロトタイプチェーン/start/main.js
+++ 060_関数とオブジェクト２/060_プロトタイプチェーン/start/main.js
@@ -1,11 +1,22 @@
 function Person(name, age) {
   this.name = name;
   this.age = age;
+  // this.helloに関数を入れる
+  this.hello = function() {
+    // 下記と区別するためにOwnPropertyを作成
+    console.log('OwnProperty: hello ' + this.name);
+  }
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
+
+
+Object.prototype.hello = function() {
+  console.log('Object: hello ' + this.name);
 }
 
 const bob = new Person('Bob', 18);
+// この結果の出力はOwnProperty: hello Bobとなる
+//　自身のプロパティでhelloが見つかったから
+// 自分のプロパティ　→　コンストラクター関数のプロトタイプのメソッド　→　他のプロトタイプへ、、、
+// 最終的にはundefinedが返ってくる
 bob.hello();
