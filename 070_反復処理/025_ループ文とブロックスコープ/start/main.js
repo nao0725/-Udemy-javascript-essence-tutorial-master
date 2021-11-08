3387e3b568117dae6f08f5a26e40359e8d254352
nao0725 <aqua.725.times@gmail.com>
2021-11-08 09:28:27 +0900

    [add] js ループ文と配列 for文

--- 070_反復処理/025_ループ文とブロックスコープ/start/main.js
+++ 070_反復処理/025_ループ文とブロックスコープ/start/main.js
@@ -1,17 +1,5 @@
-// ループ分を定義
 for(let i = 0; i < 5; i++) {
-  var j = i * 2
-  setTimeout(function(){
-    console.log(j)
-  }, 2000)
-}
-// 8が5回出力される
-// varはブロックスコープを透過するのでループの外で定義しているのと変わりないから
-// 下記でも同じ結果
-
-var b;
-for(let a = 0; a < 5; a++) {
-　 b = a * 2
+  const j = i * 2
   setTimeout(function(){
     console.log(j)
   }, 2000)
--- 070_反復処理/030_配列とループ文/start/main.js
+++ 070_反復処理/030_配列とループ文/start/main.js
@@ -1,1 +1,9 @@
+// 配列を定義
 const arry = [1, 2, 3, 4, 5];
+
+// for文を使用
+// arry.lengthで配列の要素数を表す => 5こ
+for(i = 0; i < arry.length; i++){
+  // arryの[i]番目を出力する(1,2,....)
+  console.log(arry[i]);
+}
