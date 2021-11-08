1be11aedb262de036a169621c4c7ec9e04bd9618
nao0725 <aqua.725.times@gmail.com>
2021-11-08 09:42:17 +0900

    [add] js ループ文と配列 while文

--- 070_反復処理/030_配列とループ文/start/main.js
+++ 070_反復処理/030_配列とループ文/start/main.js
@@ -1,9 +1,23 @@
 // 配列を定義
 const arry = [1, 2, 3, 4, 5];
 
-// for文を使用
-// arry.lengthで配列の要素数を表す => 5こ
-for(i = 0; i < arry.length; i++){
-  // arryの[i]番目を出力する(1,2,....)
+// while文を使用
+// 自分の回答
+// let i = 0;
+
+while(i < arry.length){
   console.log(arry[i]);
+  i++;
 }
+
+// vもiも初期値0
+let v, i = 0;
+// 後方インクリメントなのでiに値が渡ってきたら+1する
+// 0番目の値は1 v = 1で1が出力される
+// 値がtrueであればループ継続
+while(v = arry[i++]){
+  // arry[5]は6だが配列表記にないので、undefined = falseとなるのでループから抜け出せる
+  console.log(v);
+}
+
+
