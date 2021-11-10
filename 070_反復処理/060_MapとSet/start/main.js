9129c573794c9d276d256957066e01ce98c19053
nao0725 <aqua.725.times@gmail.com>
2021-11-10 10:06:53 +0900

    [add] js 反復処理 MapとSet　for..of

--- 070_反復処理/060_MapとSet/start/main.js
+++ 070_反復処理/060_MapとSet/start/main.js
@@ -1,7 +1,13 @@
 const map = new Map();
 const key1 = {};
 map.set(key1,"value1")
-// 下記のdeleteメソッドを使うと情報を削除できる
-map.delete(key1);
-// undefinedと出力される
-console.log(map.get(key1));
+// keyとvalueの値をそれぞれ渡す
+for(const [k,v] of map){
+  // {}[[Prototype]]: Object 'value1'と出力される
+  console.log(k,v);
+} 
+
+for(const a in map){
+  // for...inでは出力されない
+  console.log(a);
+}
