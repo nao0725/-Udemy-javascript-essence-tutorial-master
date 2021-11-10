a9abbb14f6ff855c01d90d43f0eb2a34549d074c
nao0725 <aqua.725.times@gmail.com>
2021-11-10 10:30:44 +0900

    [add] js 反復処理 MapとSet　Setの配列

--- 070_反復処理/060_MapとSet/start/main.js
+++ 070_反復処理/060_MapとSet/start/main.js
@@ -3,7 +3,11 @@ const set = new Set();
 const key1 = {};
 set.add(key1)
 
-
-for(const k of set){
-  console.log(k);
-}
+// setは配列の添字を指定して値を取得できない
+// よって以下の通りに取得する
+// Array.fromというメソッドを使用する
+const array = Array.from(set);
+// 下記のスプレット構文でも可
+// const array = [...set];
+console.log(array);
+// 添字を指定して出力される（0:{}）
