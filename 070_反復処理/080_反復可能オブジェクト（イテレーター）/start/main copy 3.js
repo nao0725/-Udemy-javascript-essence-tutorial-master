b23985ac27cfe8321965b4b29d93aa3209622287
nao0725 <aqua.725.times@gmail.com>
2021-11-12 10:29:39 +0900

    [add] js 反復処理　反復可能オブジェクト（イテレーター）　実装

--- 070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main copy 3.js
+++ 070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main copy 3.js
@@ -6,11 +6,16 @@ const items = {
 }
 
 Object.prototype[Symbol.iterator] = function(){
+	// このようにするとプロパティが配列になったものがkeysに格納される
 	const keys = Object.keys(this);
+	// keysの値を一つずつ取っていきたい
+	// 変数iを定義し、iが+1されるごとにkeysからキー情報を取得してそれを元に値を取得していく
 	const i = 0;
 	return {
+		// next()が呼び出されてた時に値を一つずつ取得していく
 		next(){
 			let i = keys[i];
+			// 省略して上記にi++でも可
 			i++;
 			return{
 				done: true,
--- 070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
+++ 070_反復処理/080_反復可能オブジェクト（イテレーター）/start/main.js
@@ -1,30 +1,39 @@
 
-const items = {
+// objに変数を変更
+const obj = {
 	prop1: 'value1',
 	prop2: 'value2',
 	prop3: 'value3'
 }
 
 Object.prototype[Symbol.iterator] = function(){
-	// このようにするとプロパティが配列になったものがkeysに格納される
+	// 関数内で呼ばれたthisはwindowオブジェクトを呼び出す
 	const keys = Object.keys(this);
-	// keysの値を一つずつ取っていきたい
-	// 変数iを定義し、iが+1されるごとにkeysからキー情報を取得してそれを元に値を取得していく
-	const i = 0;
+	let i = 0;
+	// ここでreturn内のthisもobjの値を取れるように設定
+	let _this = this;
 	return {
-		// next()が呼び出されてた時に値を一つずつ取得していく
 		next(){
-			let i = keys[i];
-			// 省略して上記にi++でも可
+			let key = keys[i];
 			i++;
+			// ここで取れるthisはnextという値が入ったthisが取れてくるので下記のように書かない
+			// console.log(this);
+			// 下記の_thisではスコープチェーンを辿ってnextのwindowオブジェクトを参照する
+			// console,log(_this);
 			return{
-				done: true,
-				value: 0
+				// ループの終了条件追加
+				// 配列の要素数のがiより大きくなった＝true＝終了
+				done: i > keys.length,
+				// ここでvalueを設定
+				// entriesと同じように配列で渡したいので[]でkeyと_thisのkeyをもとに値を取得する
+				value: [key, _this[key]]
 			}
 		}
 	}
 }
 // const items = Object.entries(obj);
-for (let item of items) {
-	console.log(items);
+// 分割代入でk,vとして値を渡す
+for (let [k,v] of obj) {
+	console.log(k,v);
+	// 出力結果はprop1,value1.....となる
 }
