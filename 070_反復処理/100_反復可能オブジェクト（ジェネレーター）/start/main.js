faf62c35e3ff454527414cd3983c56b0b1d2d445
nao0725 <aqua.725.times@gmail.com>
2021-11-14 19:50:46 +0900

    [add] js 反復処理　反復可能オブジェクト（ジェネレーター）書き換え

--- 070_反復処理/100_反復可能オブジェクト（ジェネレーター）/start/main.js
+++ 070_反復処理/100_反復可能オブジェクト（ジェネレーター）/start/main.js
@@ -3,3 +3,18 @@ const items = {
 	prop2: 'value2',
 	prop3: 'value3'
 }
+
+// ジェネレーター関数なのでfunctionの後ろに*をつける
+Object.prototype[Symbol.iterator] = function* (){
+	// for..inを使用
+	// thisはオブジェクトの参照を保持している
+	for(let key in this){
+		yield [key, this[key]];
+	}
+}
+// 以上で書き換え終了
+
+
+for(let [k,v] of items){
+	console.log(k, v);
+}
