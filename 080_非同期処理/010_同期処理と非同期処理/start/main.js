aca173174a4a14e85cf7e050b87250969c9b7758
nao0725 <aqua.725.times@gmail.com>
2021-11-16 08:37:47 +0900

    [add] js 非同期処理 同期処理について

--- 080_非同期処理/010_同期処理と非同期処理/start/main.js
+++ 080_非同期処理/010_同期処理と非同期処理/start/main.js
@@ -1,3 +1,6 @@
+// 関数を用意
+// sleep関数が実行されるとms分だけメインスレッドを占有できる仕組み
+// 実行されるとsleep doneと表示される
 function sleep(ms) {
   const startTime = new Date();
   while (new Date() - startTime < ms);
@@ -5,6 +8,11 @@ function sleep(ms) {
 }
 
 const btn = document.querySelector('button');
+// ブラウザ上のボタンを押すとコンソール上にbutton clickedと表示される
 btn.addEventListener('click', function(){
   console.log('button clicked');
 });
+
+// ここで3秒後に出力される
+sleep(3000)
+// この処理中にボタンを押してもsleep doneの後にbutton clickedが表示される
