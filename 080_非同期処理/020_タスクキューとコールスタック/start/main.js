cd7338bceedb454a93110eb88078b952fabea51a
nao0725 <aqua.725.times@gmail.com>
2021-11-17 07:59:15 +0900

    [add] js 非同期処理　タスクキューto

--- 080_非同期処理/020_タスクキューとコールスタック/start/main.js
+++ 080_非同期処理/020_タスクキューとコールスタック/start/main.js
@@ -1,15 +1,19 @@
 const btn = document.querySelector('button');
+// ボタンクリックするとコンソールにtask2 doneと出力
 btn.addEventListener('click', function task2() {
     console.log('task2 done');
 });
-
+// 関数aが実行されるとtask1 doneと4秒後に出力される
 function a() {
     setTimeout(function task1() {
         console.log('task1 done');
     }, 4000);
-
+// 2秒間メインスレッドを占有する処理が以下で行われ,fn a doneと出力
     const startTime = new Date();
     while (new Date() - startTime < 5000);
 
     console.log('fn a done');
 }
+
+// 関数aを実行
+a();
