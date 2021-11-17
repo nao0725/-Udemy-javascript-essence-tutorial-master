a867e26f880224562e5cc0c0cf35a004662ba3f1
nao0725 <aqua.725.times@gmail.com>
2021-11-18 08:57:47 +0900

    [add] js 非同期処理　Promise cachth

--- 080_非同期処理/050_Promise/start/main copy 3.js
+++ 080_非同期処理/050_Promise/start/main copy 3.js
@@ -1,11 +1,14 @@
 new Promise(function(resolve, reject){
   console.log("promise");
+  // rejectを実行
   reject();
 }).then(function(){
   console.log("then");
 }).then(function(){
   console.log("then");
+  // catchメソッドを使用
 }).catch(function(){
+  // thenはスキップされる
   console.log("catch");
 })
 
