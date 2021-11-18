c604ba6ed3f133ae6bf82cab1e8a6ac8fa0f8758
nao0725 <aqua.725.times@gmail.com>
2021-11-18 09:27:06 +0900

    [add] js 非同期処理　Promise 引数を与えた場合

--- 080_非同期処理/050_Promise/start/main copy 6.js
+++ 080_非同期処理/050_Promise/start/main copy 6.js
@@ -1,13 +1,20 @@
 new Promise(function(resolve, reject){
   console.log("promise");
+  // resolveの引数としてhelloとすると
   resolve("hello");
+  // reject("bye");
+  // 下記のdataがhelloとして扱われる
 }).then(function(data){
+  // then helloと出力
   console.log("then " + data);
+  // ここで値を返却することで2個目のthenもhelloと出力される
   return data;
 }).then(function(data){
   console.log("then " + data);
 }).catch(function(data){
+  // rejectが使われていた場合catchが呼ばれる
   console.log("catch " + data);
+  // finallyは引数を取れない
 }).finally(function(){
   console.log("finally");
 })
