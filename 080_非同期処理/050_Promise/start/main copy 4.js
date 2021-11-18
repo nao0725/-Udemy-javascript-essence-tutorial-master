8ec720d7d9e034e0d1cb148f2eea5426f9ace362
nao0725 <aqua.725.times@gmail.com>
2021-11-18 09:03:38 +0900

    [add] js 非同期処理　Promise throw

---
+++ 080_非同期処理/050_Promise/start/main copy 4.js
@@ -0,0 +1,12 @@
+new Promise(function(resolve, reject){
+  console.log("promise");
+  resolve();
+}).then(function(){
+  console.log("then");
+}).then(function(){
+  console.log("then");
+}).catch(function(){
+  console.log("catch");
+})
+
+console.log("global end")
