6ce2ea8b109b8d04e5b67b649efb263cebe1e9c3
nao0725 <aqua.725.times@gmail.com>
2021-11-19 18:45:13 +0900

    [add] js 非同期処理　Promise チェーン

--- 080_非同期処理/060_Promiseチェーン/start/main.js
+++ 080_非同期処理/060_Promiseチェーン/start/main.js
@@ -1,6 +1,50 @@
-function sleep(callback, val) {
-  setTimeout(function() {
-    console.log(val++);
-    callback(val);
-  }, 1000);
+function sleep(val) {
+  // sleepに値が返却されるようにreturnを使う
+  // Promiseの中でsetTimeoutを使う
+  return new Promise(function(resolve){
+    setTimeout(function() {
+      console.log(val++);
+      resolve(val);
+    }, 1000);
+  });
 }
+
+// sleepのvalを実行
+// sleepにresolveの値が入っているのでthenメソッドを使ってコールバック関数を追加
+sleep(0).then(function(val){
+  //Promiseのチェーンを繋げる
+  // Promiseのインスタンスを使ってPromiseをチェーンする
+  // 1秒ごとに1,2,と出力される
+  return sleep(val);
+}).then(function(val){
+  return sleep(val);
+}).then(function(val){
+  return sleep(val);
+}).then(function(val){
+  return sleep(val);
+})
+function sleep(val) {
+  // sleepに値が返却されるようにreturnを使う
+  // Promiseの中でsetTimeoutを使う
+  return new Promise(function(resolve){
+    setTimeout(function() {
+      console.log(val++);
+      resolve(val);
+    }, 1000);
+  });
+}
+
+// sleepのvalを実行
+// sleepにresolveの値が入っているのでthenメソッドを使ってコールバック関数を追加
+sleep(0).then(function(val){
+  //Promiseのチェーンを繋げる
+  // Promiseのインスタンスを使ってPromiseをチェーンする
+  // 1秒ごとに1,2,と出力される
+  return sleep(val);
+}).then(function(val){
+  return sleep(val);
+}).then(function(val){
+  return sleep(val);
+}).then(function(val){
+  return sleep(val);
+})
