452e2dec7b39b27570dcd6b1984ecce1d35d2554
nao0725 <aqua.725.times@gmail.com>
2021-11-21 15:24:38 +0900

    [add] js 非同期処理　Promiseと並列処理 return後のall

--- 080_非同期処理/070_Promiseと並列処理/start/main copy 3.js
+++ 080_非同期処理/070_Promiseと並列処理/start/main copy 3.js
@@ -12,8 +12,11 @@ Promise.all([sleep(2), sleep(3), sleep(4)])
   console.log(data);
 });
 sleep(0).then(function(val) {
+  // 上記のPromise.allの部分を下記のreturnのところで置換する
+  // 次のthenメソッドも下記Promiseの処理が終わってから処理される
   return Promise.all([sleep(2), sleep(3), sleep(4)])
 }).then(function(val) {
+  // 出力すると先のPromisee.allの値が全て返ってくる
   console.log(val)
   return sleep(val);
 }).then(function(val) {
