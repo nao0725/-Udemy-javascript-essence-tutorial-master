02be5ff29e7e6288e81cfb1109444d83c9770d3d
nao0725 <aqua.725.times@gmail.com>
2021-11-22 08:35:01 +0900

    [add] js 非同期処理　マイクロタスクとマクロタスク

--- 080_非同期処理/080_MacrotasksとMicrotasks（Part. 1）/start/main.js
+++ 080_非同期処理/080_MacrotasksとMicrotasks（Part. 1）/start/main.js
@@ -1,12 +1,19 @@
+// 非同期処理でタスクキューで処理される
+// グローバルコンテキストが呼ばれた後に実行される
 setTimeout(function task1() {
   console.log('task1');
 });
 
 new Promise(function promise(resolve) {
+  // ここは同期的に処理される
+  //　だから一番最初に表示される
   console.log('promise');
   resolve();
 }).then(function job1() {
+  // ここも非同期処理なのでグローバルコンテキスト実行後
+  // マイクロタスクが先に処理されるのでこちらが先に出力される
   console.log('job1');
 });
 
+// 先の同期処理"promise"後表示
 console.log('global end');
