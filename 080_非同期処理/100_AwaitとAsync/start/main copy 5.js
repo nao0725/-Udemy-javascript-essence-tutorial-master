cc2ea8010054525fd05ad2a11685466b4714d595
nao0725 <aqua.725.times@gmail.com>
2021-11-23 17:56:15 +0900

    [add] js 非同期処理　AwaitとAsync　thenとcatchを使用した際の処理

---
+++ 080_非同期処理/100_AwaitとAsync/start/main copy 5.js
@@ -0,0 +1,36 @@
+function sleep(val) {
+  return new Promise(function(resolve) {
+    setTimeout(function() {
+      console.log(val++);
+      resolve(val);
+    }, 1000);
+  });
+}
+
+async function init() {
+  let val = await sleep(0);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  // ここでreturnを宣言すると
+  return val
+}
+
+// ここの引数でvalが使える
+init().then(function(val){
+  // そうすると5番目の出力が"hello 5"となる
+  console.log("hello " + val);
+})
+
+// sleep(0).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// }).then(function(val) {
+//   return sleep(val);
+// })
--- 080_非同期処理/100_AwaitとAsync/start/main.js
+++ 080_非同期処理/100_AwaitとAsync/start/main.js
@@ -7,14 +7,23 @@ function sleep(val) {
   });
 }
 
-sleep(0).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
+async function init() {
+  let val = await sleep(0);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  val = await sleep(val);
+  // ここでthrowを宣言すると
+  throw new Error();
+  return val
+}
+
+init().then(function(val){
+  console.log("hello " + val);
+  // catchに処理が移る
+}).catch(function(e){
+  // 5の個所でエラー表示がされる
+  console.error(e);
 })
+
+// 基本的には、Promiseと挙動が同じ
