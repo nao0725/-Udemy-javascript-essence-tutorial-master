a8f4dbe6f99dad55c30d2a555909e9b4ae249031
nao0725 <aqua.725.times@gmail.com>
2021-11-24 09:29:18 +0900

    [add] js 非同期処理　fetch　出力結果の確認

--- 080_非同期処理/110_fetch/start/main.js
+++ 080_非同期処理/110_fetch/start/main.js
@@ -1,9 +1,7 @@
-// 第一引数にリクエスト先のURLを入力（今回はuser.jsonファイル）
-// fetch("users.json")
-// どのような戻り値が帰ってくるのかコンソールで確認
-// console.log(fetch(`users.json`))
-// このようにするとPromiseが返ってきていることがわかるのでthenメソッドが使える
 fetch(`users.json`).then(function(response){
-  // サーバーから返ってきたデータを格納している
-  console.log(response);
+  // コンソールからjsonというメソッドが使えることがわかる
+  return response.json();
+}).then(function(Json){
+  // この引数Jsonにデータが入っているか確認する
+    console.log(Json);
 })
