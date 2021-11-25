eab69c547fbbd5a9e79158df828949a7a54f7fa0
nao0725 <aqua.725.times@gmail.com>
2021-11-25 17:51:02 +0900

    [add] js 非同期処理　例外処理とエラー　実践的な書き方

--- 080_非同期処理/120_例外処理とエラー/start/main.js
+++ 080_非同期処理/120_例外処理とエラー/start/main.js
@@ -1,23 +1,31 @@
-// 動かしたいコードをtryの中に記述していく
-try{
-  console.log("start");
-  // エラーを出力する　→　catchに移行する
-  throw new Error("error message");
-  // 上記で処理がcatchに移行するので下記endは出力されない
-  console.log("end")
-} catch(e) {
-  console.error(e);
-} finally {
-  // 出力される終了処理
-  console.log('bye');
+async function fetchUsers() {
+  const response = await fetch('users.json');
+  // 条件分岐でデータがうまく渡っている場合そうでない場合で分ける
+  if (response.ok){
+    const json = await response.json();
+    // ここでjsonファイルが0だった場合にthrowでエラー処理をcatchに移行したい
+    // jsonファイルのlength（配列の長さ）が0だった場合falseなので!演算子でtrueにする
+    if(!json.length){
+      throw new Error("no data found")
+    }
+    return json;
+  } 
 }
 
-// async function fetchUsers() {
-//   const response = await fetch('users.json');
-//   const json = await response.json();
-//   for(const user of json) {
-//     console.log(`I'm ${user.name}, ${user.age} years old`)
-//   }
-// }
+async function init(){
+  // 上記の内容をtryで捌く
+  try {
+    const users = await fetchUsers();
+    for(const user of users) {
+        console.log(`I'm ${user.name}, ${user.age} years old`)
+    }
+  }catch(e){
+    // ここでthrowを受けることでno data foundとエラー表示される
+    console.error(e);
+  }finally{
+    // 終了処理はどちらにせよ出力される
+    console.log('bye');
+  }
+}
 
-// fetchUsers();
+init();
