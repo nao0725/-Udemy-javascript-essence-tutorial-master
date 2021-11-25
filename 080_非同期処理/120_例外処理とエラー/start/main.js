df77c73df6e10394c9df328fae7bb2e0da44bd30
nao0725 <aqua.725.times@gmail.com>
2021-11-25 17:20:03 +0900

    [add] js 非同期処理　例外処理とエラー　基本構文

--- 080_非同期処理/120_例外処理とエラー/start/main.js
+++ 080_非同期処理/120_例外処理とエラー/start/main.js
@@ -1,4 +1,16 @@
-
+// 動かしたいコードをtryの中に記述していく
+try{
+  console.log("start");
+  // エラーを出力する　→　catchに移行する
+  throw new Error("error message");
+  // 上記で処理がcatchに移行するので下記endは出力されない
+  console.log("end")
+} catch(e) {
+  console.error(e);
+} finally {
+  // 出力される終了処理
+  console.log('bye');
+}
 
 // async function fetchUsers() {
 //   const response = await fetch('users.json');
