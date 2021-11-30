3925aba7ff9f46d03a854f6ea424decce0031d2a
nao0725 <aqua.725.times@gmail.com>
2021-11-30 09:52:46 +0900

    [add] js モジュール　Strictモード　クラスの関係

--- 090_モジュラーJavaScript/060_Strictモードとクラス/start/main.js
+++ 090_モジュラーJavaScript/060_Strictモードとクラス/start/main.js
@@ -1,8 +1,18 @@
+// 下記は両方ともundefinedと出力される
+// コンストラクターもメソッドも内部で宣言した関数内のthisが表示されない
 class C {
     constructor() {
+        function fn() {
+            console.log(this)
+        }
+        fn();
     }
 
     method() {
+        function fn() {
+            console.log(this)
+        }
+        fn();
     }
 }
 
