afa9f827ca6c21434266e6a2b7a0925a1a7ff91f
nao0725 <aqua.725.times@gmail.com>
2021-12-02 08:50:46 +0900

    [add] js さまざまなオブジェクト get

---
+++ 100_様々なオブジェクト/010_Proxy/start/main.js
@@ -0,0 +1,14 @@
+const targetObj = { a: 0 };
+const handler = {
+  // getでは値の取得が行われた際の検知ができるようになる
+  // 値の取得なので新しい値は渡ってこない　＝　第三引数は削除（value）
+  get: function (target, prop, receiver) {
+    console.log(`[get]: ${prop}`);
+    // returnで値を返す
+    return target[prop];
+  }
+}
+
+const proxy = new Proxy(targetObj, handler)
+// [get]:aと出力される
+proxy.a;
