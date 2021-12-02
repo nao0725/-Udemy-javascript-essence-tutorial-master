8254cff510e2ccdf8290586681e390fb8432587c
nao0725 <aqua.725.times@gmail.com>
2021-12-02 09:04:24 +0900

    [add] js さまざまなオブジェクト delete

--- 100_様々なオブジェクト/010_Proxy/start/main.js
+++ 100_様々なオブジェクト/010_Proxy/start/main.js
@@ -1,14 +1,21 @@
 const targetObj = { a: 0 };
 const handler = {
-  // getでは値の取得が行われた際の検知ができるようになる
-  // 値の取得なので新しい値は渡ってこない　＝　第三引数は削除（value）
   get: function (target, prop, receiver) {
     console.log(`[get]: ${prop}`);
-    // returnで値を返す
     return target[prop];
+  },
+  // 下記のdeleteでは値が削除された際の検知を行う
+  // deletePropertyの場合にはreceiverは渡ってこないので削除
+  deleteProperty: function (target, prop) {
+    console.log(`[delete]: ${prop}`);
+    // deleteとして値を削除
+    delete target[prop];
   }
 }
 
-const proxy = new Proxy(targetObj, handler)
-// [get]:aと出力される
+const proxy = new Proxy(targetObj, handler);
 proxy.a;
+// delete aと出力される
+delete proxy.a;
+// targetObjの中身をコンソールで確認してみるとオブジェクトの中身も削除されている
+
